32ecae396686393e88d6be7357b1fe20988bde84
Guillaume MICHEL <guillaume.michel@epitech.eu>
2024-09-24 22:08:18 +0200

    ADD -r flag

--- build.sh
+++ build.sh
@@ -2,6 +2,23 @@
 
 # This script configures and builds the project using CMake
 
+# Default build type (Debug by default)
+BUILD_TYPE="Debug"
+
+# Parse command-line arguments for build type (Release if specified)
+while getopts "r" opt; do
+  case ${opt} in
+    r )
+      BUILD_TYPE="Release"
+      ;;
+    \? )
+      echo "Usage: $0 [-r]"
+      echo "  -r    Use Release build type (default is Debug)"
+      exit 1
+      ;;
+  esac
+done
+
 # Create the build directory if it doesn't exist
 if [ ! -d "build" ]; then
     echo "Creating the build directory..."
@@ -9,24 +26,21 @@ if [ ! -d "build" ]; then
 fi
 
 # Navigate to the build directory, exit if cd fails
-cd build || { echo "Failed to change directory to 'build'. Exiting..."; exit 1; }
+if ! cd build; then
+    echo "Failed to change directory to 'build'. Exiting..."
+    exit 1
+fi
 
 # Run CMake to generate build files
-echo "Configuring the project with CMake..."
-cmake ..
-
-# Check if CMake succeeded
-if [ $? -ne 0 ]; then
+echo "Configuring the project with CMake (Build type: ${BUILD_TYPE})..."
+if ! cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..; then
     echo "CMake configuration failed. Please check the errors above."
     exit 1
 fi
 
 # Run the build process (use make for Unix-like systems)
 echo "Building the project..."
-make
-
-# Check if make succeeded
-if [ $? -ne 0 ]; then
+if ! make; then
     echo "Build failed. Please check the errors above."
     exit 1
 fi
